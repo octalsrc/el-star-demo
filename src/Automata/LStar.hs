6fcb97d81726a879211431a19337d3d2cbe957eb
octalsrc <nicklewchenko92@gmail.com>
2016-12-12 14:41:55 -0700

    state datatypes

--- src/Automata/LStar.hs
+++ src/Automata/LStar.hs
@@ -16,6 +16,9 @@ import Data.Maybe (fromJust)
 import Data.Monoid ((<>))
 import Data.List (nub)
 
+import Control.Monad.Reader
+import Control.Monad.State
+
 import Automata.DFA
 
 ----------------------------------------------------------------------
@@ -94,6 +97,12 @@ askDebug (AskTeacher ioa) = ioa >>= print
 ----------------------------------------------------------------------
 -- LEARNER
 
+type LState t a = StateT (Pos a,[Pos a]) (ReaderT a t)
+
+data Pos a = PosUnf (Unfilled a)
+           | PosUnc (Unchecked a)
+           | PosClo (Closed a)
+
 type OTable a = Map (Prefix a, Suffix a) Bool
 
 class Notes n where
@@ -101,17 +110,43 @@ class Notes n where
   getE :: AClass a => n a -> [Suffix a]
   getT :: AClass a => n a -> OTable a
 
-data Unchecked a = Unchecked { getS' :: [Prefix a]
-                             , getE' :: [Suffix a]
-                             , getT' :: OTable a }
-                             
-deriving instance Show (AElem a) => Show (Unchecked a)
+data Unfilled a = Unfilled { getS' :: [Prefix a]
+                           , getE' :: [Suffix a]
+                           , getT' :: OTable a }
 
+deriving instance (Ord (AElem a), Read (AElem a)) => Read (Unfilled a)
+deriving instance Show (AElem a) => Show (Unfilled a)
+deriving instance Eq (AElem a) => Eq (Unfilled a)
+deriving instance Ord (AElem a) => Ord (Unfilled a)
 
-instance Notes Unchecked where
+instance Notes Unfilled where
   getS = getS'
   getE = getE'
   getT = getT'
+
+newtype Unchecked a = Unchecked { fromUnchecked :: Unfilled a }
+
+deriving instance (Ord (AElem a), Read (AElem a)) => Read (Unchecked a)
+deriving instance Show (AElem a) => Show (Unchecked a)
+deriving instance Eq (AElem a) => Eq (Unchecked a)
+deriving instance Ord (AElem a) => Ord (Unchecked a)
+
+instance Notes Unchecked where
+  getS = getS . fromUnchecked
+  getE = getE . fromUnchecked
+  getT = getT . fromUnchecked
+
+newtype Closed a = Closed { fromClosed :: Unchecked a } 
+
+deriving instance (Ord (AElem a), Read (AElem a)) => Read (Closed a)
+deriving instance Show (AElem a) => Show (Closed a)
+deriving instance Eq (AElem a) => Eq (Closed a)
+deriving instance Ord (AElem a) => Ord (Closed a)
+
+instance Notes Closed where
+  getS (Closed n) = getS n
+  getE (Closed n) = getE n
+  getT (Closed n) = getT n
 
 addEntry :: (Functor t, Teacher t a) 
          => a -> (Prefix a, Suffix a) -> OTable a -> t (OTable a)
@@ -134,7 +169,7 @@ extendS a ps n = let (s,e,t) = (getS n, getE n, getT n)
                      newS = s ++ ps
                      newEntries = [(pr,sf) | pr <- (allPs a newS), sf <- e]
                      newTable = foldM (findEntry a) t newEntries
-                 in Unchecked newS e <$> newTable
+                 in Unchecked <$> Unfilled newS e <$> newTable
 
 findEntry :: (Monad t, Teacher t a) 
           => a -> OTable a -> (Prefix a, Suffix a) -> t (OTable a)
@@ -156,24 +191,17 @@ extendE a ss n = let (s,e,t) = (getS n, getE n, getT n)
                      newE = e ++ ss
                      newEntries = [(pr,sf) | pr <- (allPs a s), sf <- newE]
                      newTable = foldM (findEntry a) t newEntries
-                 in Unchecked s newE <$> newTable
+                 in Unchecked <$> Unfilled s newE <$> newTable
 
 -- TODO: combine extendE and extendS into one function
 
 initNotes :: (Monad t, Teacher t a) => a -> t (Unchecked a)
 initNotes a = 
-  return (Unchecked [] [] empty) 
+  return (Unchecked $ Unfilled [] [] empty)
   >>= extendE a ([eps a] ++ (map (\e -> [e]) . S.toList . elements $ a))
   >>= extendS a [eps a]
 
-newtype Closed n a = Closed (n a) deriving (Read, Show, Eq, Ord)
-
-instance (Notes n) => Notes (Closed n) where
-  getS (Closed n) = getS n
-  getE (Closed n) = getE n
-  getT (Closed n) = getT n
-
-checkClosed :: (AClass a, Notes n) => a -> n a -> Either (Prefix a) (Closed n a)
+checkClosed :: (AClass a) => a -> Unchecked a -> Either (Prefix a) (Closed a)
 checkClosed a n = let (s,e,t) = (getS n, getE n, getT n)
                       getRow' = getRow a t e 
                       srows = map getRow' s
@@ -187,13 +215,13 @@ getRow :: (AClass a) => a -> OTable a -> [Suffix a] -> Prefix a -> [Bool]
 getRow _ t ss p = map (\s -> t M.! (p,s)) ss
 
 makeClosed :: (Monad t, Teacher t a) 
-           => a -> Unchecked a -> t (Closed Unchecked a)
+           => a -> Unchecked a -> t (Closed a)
 makeClosed a n = 
   case checkClosed a n of
     Left p -> extendS a [p] n >>= makeClosed a
     Right c -> return c
 
-conjecture :: (AClass a, Notes n) => a -> Closed n a -> DFA a
+conjecture :: (AClass a) => a -> Closed a -> DFA a
 conjecture a n = 
   let (s,e,t) = (getS n, getE n, getT n)
       getRow' = getRow a t e
@@ -247,157 +275,3 @@ starLoop a n =
      case res of
        Just ce -> processCE a cl ce >>= starLoop a
        Nothing -> return dfa
-
--- extend :: (Teacher t a) => [Prefix a] -> [Suffix a] -> Unchecked a -> t (Unchecked a)
--- extend ps ss (Unchecked s e t) = fill (s ++ ps) (e ++ ss) t
-
--- fill :: (Teacher t a) => a -> [Prefix a] -> [Suffix a] -> OTable a -> t (Unchecked a)
--- fill a ps ss t = let vs = mapM (\p -> memberQ a p) ps
---                      tbl = foldr insert t <$> vs
---                  in undefined
-
--- initNotesW :: Alphabet a => a -> Unchecked a
--- initNotesW _ = Unchecked [] [] empty
-
--- initialNotes :: (Teacher a t) => a -> t (Unchecked a)
--- initialNotes as = extend [eps] [eps] (initNotesW as)
-
-
--- rebase :: Notes a n => n -> Unchecked a
--- rebase n = Unchecked (getS n) (getE n) (getT n)
-
--- proven :: (Notes a n1, Notes a n2) => (Unchecked a -> n2) -> n1 -> n2
--- proven f n = f (rebase n)
-
--- extend :: (Teacher a t, Notes a n) 
---        => [Prefix a] -> [Suffix a] -> n -> t (Unchecked a)
--- extend ss es nts =
---   let nqs = [ (s,e) | s <- sdota ss, e <- es ]      
---       getNewTbl = foldM addEntry (getT nts) nqs
---       newSS = (getS nts) ++ ss
---       newES = (getE nts) ++ es
---   in getNewTbl >>= (\newTbl -> return (Unchecked newSS newES newTbl))
-
--- addEntry :: Teacher a t => OTable a -> (Prefix a, Suffix a) -> t (OTable a)
--- addEntry tbl (s,e) =
---   if member (s,e) tbl -- is this already in our table?
---      then return tbl -- then we already know this
---      else do r <- memberQ (s <> e) -- then we need to ask the teacher
---              return (insert (s,e) r tbl)
-
--- -- S `dot` A
--- sdota :: Alphabet a => [Prefix a] -> [Prefix a]
--- sdota ss = [ app s a | s <- ss, a <- setA ]
-
--- newtype Consistent a = Consistent (Unchecked a)
-
--- instance Alphabet a => Notes a (Consistent a) where
---   getS (Consistent u) = getS' u
---   getE (Consistent u) = getE' u
---   getT (Consistent u) = getT' u
-
--- consistent :: (Teacher a t, Notes a n)
---            => n
---            -> Either (t (Consistent a)) (Consistent a)
--- consistent n = case consCEs n of
---                  (a:_) -> Left (mkConsistent a n)
---                  _ -> Right (proven Consistent n)
-
--- mkConsistent :: (Teacher a t, Notes a n) 
---              => Suffix a
---              -> n
---              -> t (Consistent a)
--- mkConsistent e n = do n2 <- extend [] [e] n
---                       case consistent n of
---                         Left getC -> getC
---                         Right c -> return c
-
--- tblRow :: Alphabet a => OTable a -> [Suffix a] -> Prefix a -> [Bool]
--- tblRow t es s = [ fromJust (M.lookup (s,e) t) | e <- es ]
-
--- -- counterexamples to consistency
--- consCEs :: Notes a n => n -> [Suffix a]
--- consCEs n = let setS = getS n
---                 setE = getE n
---                 row = tblRow (getT n) setE
---                 sc s1 s2 = row s1 == row s2
---                 ec s1 s2 a e =
---                   row (app s1 a <> e)
---                   /= row (app s2 a <> e)
---             in [ str a <> e | s1 <- setS
---                             , s2 <- setS
---                             , a <- setA
---                             , e <- setE
---                             , sc s1 s2
---                             , ec s1 s2 a e ]
-
--- newtype Closed a = Closed (Unchecked a)
-
--- instance Alphabet a => Notes a (Closed a) where
---   getS (Closed u) = getS' u
---   getE (Closed u) = getE' u
---   getT (Closed u) = getT' u
-
--- closed :: (Teacher a t, Notes a n)
---        => n
---        -> Either (t (Closed a)) (Closed a)
--- closed n = case closCEs n  of
---              a:_ -> Left (mkClosed a n)
---              _ -> Right (proven Closed n)
-
--- closCEs :: Notes a n => n -> [Prefix a]
--- closCEs n = let setS = getS n
---                 setE = getE n
---                 row = tblRow (getT n) setE
---                 rows = map row setS
---             in [ app s a | s <- setS
---                          , a <- setA
---                          , not (elem (row (app s a)) rows)]
-
--- mkClosed :: (Teacher a t, Notes a n) 
---          => Prefix a
---          -> n
---          -> t (Closed a)
--- mkClosed p n = do n2 <- extend [p] [] n
---                   case closed n of
---                     Left getC -> getC
---                     Right c -> return c
-
--- newtype CAndC a = CAndC (Unchecked a)
-
--- instance Alphabet a => Notes a (CAndC a) where
---   getS (CAndC u) = getS' u
---   getE (CAndC u) = getE' u
---   getT (CAndC u) = getT' u
-
--- fromCons :: (Teacher a t)
---          => Consistent a
---          -> Either (t (Closed a)) (CAndC a)
--- fromCons n = case closed n of
---                Right c -> Right (proven CAndC c)
---                Left p -> Left p
-
--- fromClos :: (Teacher a t)
---          => Closed a
---          -> Either (t (Consistent a)) (CAndC a)
--- fromClos n = case consistent n of
---                Right c -> Right (proven CAndC c)
---                Left p -> Left p
-
--- conjecture :: Teacher a t => CAndC a -> t (Maybe (Prefix a))
--- conjecture = undefined
-
--- conjecture' :: Alphabet a => [a] -> CAndC a -> DFA a
--- conjecture' as c = let setS = getSw as c
---                        setE = getEw as c
---                        tbl = getTw as c
---                        row = tblRow tbl setE
---                        setQ = nub [ row s | s <- setS ]
---                        qZ = row eps
---                        -- setF ?
---                    in (M.fromList (zip (zip (setQ) as) setQ), [[]])
-
--- -- type DFA a = (Map ([Bool], a) [Bool], [[Bool]])
-
--- elstar :: Teacher a t => [a] -> t (DFA a)
--- elstar as = undefined
